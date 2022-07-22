b155e252c990f77b931e4bac3d55ab983fe45482
mojtabafallah13 <mojtaba.fallah1373@gmail.com>
2022-07-22 11:08:20 +0430

    init

---
+++ style_studio.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Style Studio
+Plugin URI: https://github.com/mojtabafallah/style-studio
+Description: برای ارتباط بین برنامه حسابداری آرتیسا و وردپرس
+Version: 1.0.0
+Author: Mojtaba Fallah
+Author URI: https://github.com/mojtabafallah
+Text Domain: stylestudio
+*/
